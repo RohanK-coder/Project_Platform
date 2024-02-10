ebf3914846a8e297a2a274f08706f8d67e28cbee
rohank <kyrohan7730@gmail.com>
2024-02-10 20:41:17 +0530

    sign-up page remastered

--- Platform Page/src/components/Signup/Signup.jsx
+++ Platform Page/src/components/Signup/Signup.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Select, Option } from "@material-tailwind/react";
-import { Button } from "@material-tailwind/react";
+
 import Tabs from '../Tabs/TabsInside'
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 export default function Signup() {
   const [value, setValue] = React.useState("react");
   return (
@@ -19,7 +26,9 @@ in understanding and solving real world problems.</div>
       </div>
       
       <div className='dropdown-select'>
-      <div>
+      <div className='dropdown'>
+      <div className='dropdown-issue'>
+      <div className='mr-7'>
       <Select
         label="Select Persona"
         value={value}
@@ -32,7 +41,7 @@ in understanding and solving real world problems.</div>
         <Option value="svelte">Non-Profit Organization</Option>
       </Select>
       </div>
-      <div>
+      <div className='mr-10'>
       <Select
         label="Select Region"
         value={value}
@@ -45,7 +54,7 @@ in understanding and solving real world problems.</div>
         
       </Select>
       </div>
-      <div>
+      <div className='mr-10'>
       <Select
         label="Select Sectors"
         value={value}
@@ -61,9 +70,84 @@ in understanding and solving real world problems.</div>
         <Option value="svelte">Livelihood</Option>
       </Select>
       </div>
-      <Button color="blue">Continue</Button>
       </div>
-      <Tabs/>
+      <Card color="transparent" shadow={false}>
+      <Typography variant="h4" color="blue-gray">
+        Sign Up
+      </Typography>
+      <Typography color="gray" className="mt-1 font-normal">
+        Nice to meet you! Enter your details to register.
+      </Typography>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <div className="mb-1 flex flex-col gap-6">
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Name
+          </Typography>
+          <Input
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Email
+          </Typography>
+          <Input
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Password
+          </Typography>
+          <Input
+            type="password"
+            size="lg"
+            placeholder="********"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+        </div>
+        <Checkbox
+          label={
+            <Typography
+              variant="small"
+              color="gray"
+              className="flex items-center font-normal"
+            >
+              I agree the
+              <a
+                href="#"
+                className="font-medium transition-colors hover:text-gray-900"
+              >
+                &nbsp;Terms and Conditions
+              </a>
+            </Typography>
+          }
+          containerProps={{ className: "-ml-2.5" }}
+        />
+        <Button className="mt-6" fullWidth>
+          sign up
+        </Button>
+        <Typography color="gray" className="mt-4 text-center font-normal">
+          Already have an account?{" "}
+          <a href="#" className="font-medium text-gray-900">
+            Sign In
+          </a>
+        </Typography>
+      </form>
+    </Card>
+      </div>
+      
+    </div>
+      
     
 
 
