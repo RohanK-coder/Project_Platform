4b279d3ca5fbeec731aed00d621c628336234486
Venkat-Kancherla <devisri.21bce9122@vitapstudent.ac.in>
2024-02-16 01:56:43 +0530

    Changed images

--- Platform Page/src/components/Home/RuralGC.jsx
+++ Platform Page/src/components/Home/RuralGC.jsx
@@ -151,19 +151,19 @@ export default function CardSlider() {
 const data = [
   {
     name: 'Turmeric',
-    img: '/src/assets/turmeric.jpg',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1707881649/GC/vs1fnpc1mydaj7wkhnxp.jpg',
     review: 'How can Meghalaya market its spice treasure “turmeric” to global...',
     anchor:"/details/65ca13d77d462abcc25591ad",
   },
   {
     name: 'Horticulture',
-    img: '/src/assets/horticulture.jpg',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1707881649/GC/ubk6amkb4mtkwikr5dzh.jpg',
     review: 'Horticulture: How can Meghalaya access global markets to distribute their..',
     anchor:"/details/65cc7bc5cbc1cfc501ba3f49",
   },
   {
     name: 'Digital Healthcare',
-    img: '/src/assets/digital.jpg',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1707881649/GC/qteb0vxw7t9rlqymt7im.jpg',
     review: 'Healthcare: How can high quality “AI-led healthcare services” be made...',
     anchor:"/details/65cc7cbfcbc1cfc501ba3f4a",
   },
@@ -171,19 +171,19 @@ const data = [
 const data2 = [
   {
     name: 'Waste Management',
-    img: '/src/assets/waste.jpg',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1708028384/i4fjwh0dra0raqrc1chx.jpg',
     anchor:"/research-project/65cc8547cbc1cfc501ba3f55",
     review: 'ProjectType: Research',
   },
   {
     name: 'Access to Clean Cooking Fuel',
-    img: '/src/assets/cleanFuel.jpg',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1708028388/lhhdhk97mdymhr1xmpcy.jpg',
     anchor:"/research-project/65cc8547cbc1cfc501ba3f55",
     review: 'ProjectType: Research',
   },
   {
     name: 'Affordable Housing',
-    img: '/src/assets/housing.jpg',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1708028386/kggltijb80fhn5oo8pk7.jpg',
     anchor:"/research-project/65cc8547cbc1cfc501ba3f55",
     review: 'ProjectType: Research',
   },
@@ -191,20 +191,20 @@ const data2 = [
 const data3 = [
   {
     name: 'Salesforce Trailblazers Labs',
-    img: '/src/assets/salesforce.jpg',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1708028605/ylckxu6ns5snxvuq64wq.jpg',
     anchor:"/village-projects",
     review: 'Salesforce, headquartered in San Francisco, California, is setting up two Trailblazer labs in two selected Smart Village Movement pilot villages in the East Khasi Hills District,..',
   },
   {
     name: 'Gramin Representative arrival for ground assessment',
-    img: '/src/assets/gramin.avif',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1708028605/iepvrwtufocnewtdhn2y.avif',
     anchor:"/village-project/65cce272a3ff5a5d14a9ddb4",
     review: 'Activity Date: 19-Jan-2021',
     
   },
   {
     name: 'Hygge Energy Microgrid',
-    img: '/src/assets/hygge.jpg',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1708028606/jsfmvdm1cmbpto9x6ihw.jpg',
     anchor:"/research-project/65cc8547cbc1cfc501ba3f55",
     review: 'ProjectType: Collaboration',
   },
@@ -212,19 +212,19 @@ const data3 = [
 const data4 = [
   {
     name: 'Tulaa',
-    img: '/src/assets/tulaa.png',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1707885861/Technologies/sek3hdeufauovtsdu7wl.png',
     anchor:"/technologies/65cba5cd7d462abcc2146578 ",
     review: 'Salesforce, headquartered in San Francisco, California, is setting up two Trailblazer labs in two selected Smart Village Movement pilot villages in the East Khasi Hills District,..',
   },
   {
     name: 'Doctor on Demand',
-    img: '/src/assets/dr.png',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1707885861/Technologies/xgsaxis59j5uznrg8qwa.png',
     anchor:"/technologies/65cba5cd7d462abcc214657a",
     review: 'Activity Date: 19-Jan-2021',
   },
   {
     name: 'Waste Ventures India',
-    img: '/src/assets/wasteventures.png',
+    img: 'https://res.cloudinary.com/duad230uy/image/upload/v1707885861/Technologies/lpvj8ewzhqqmc54nphn2.png',
     anchor:"/technologies/65cba5cd7d462abcc214657e ",
     review: 'ProjectType: Collaboration',
   },
